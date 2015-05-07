eb7f7ecebdf64a7657f2f3794132171e5c340000
Gang Su <m.sugang@gmail.com>
2015-05-06 17:51:19 -0700

    Update GS_script.R
    Tried SVM / RF / Deeplearning.
    Need to do variable selection, balance weights, cross-validation

--- Otto/GS_script.R
+++ Otto/GS_script.R
@@ -14,7 +14,18 @@ train.class <- as.factor(train$target)
 test.features <- test[,c(-1,-95)]
 
 #quick run, error ~ 22%
-rf.classifier.full <- randomForest(train.features, train.class, ntree=25, importance=TRUE)
+rf.classifier.full <- randomForest(train.features, train.class, ntree=50, importance=TRUE)
+rf.classifier.importance <-  importance(rf.classifier.full, type=1)
+
+#choose importance >= 10
+rf.vs.impSel <- rf.classifier.importance[,1] >= 10
+rf.classifier.vs.impSel <- randomForest(train.features[,rf.vs.impSel], train.class, ntree=50) #retrain using selected features
+
+rf.classifier.vs.impSel.predict <-predict(rf.classifier.vs.impSel, test[,c(-1)][,rf.vs.impSel], type="prob")
+submission[,2:10] <- rf.classifier.vs.impSel.predict #varsel w/ rF 20% error
+
+
+
 
 #variable selection -simple
 library(varSelRF)
@@ -32,6 +43,22 @@ svm.predict.full.test <- predict(svm.classifier.full, test.features)
 
 svm.predict.output <- adply(as.vector(svm.predict.full.test), 1, function(x){(classes == x)*1}) #plyr function to 
 colnames(svm.predict.output) <- c('id',classes)
+
+#svm using nu-classification
+svm.classifier.01 <- svm(train.features, train.class, type="nu-classification", kernal='linear', nu=0.05, cross=5)
+
+#consider weight balancing issue:
+wt <- 10000/table(train$target) # run with weight
+
+
+
+
+###############################################################################################################
+# library(penalizedSVM) #this only works on single class classification methods.
+# svm.classifier.p <- lpsvm(train.features, train.class, k=5, nu=0.05, output=1)
+
+
+
 
 #deep-learning, need to set up using h2o.
 #########################################
@@ -66,3 +93,23 @@ dl.classifier.full <- h2o.deeplearning(
 dl.vs <- dl.classifier.full@model$varimp
 
 #h2o random forest
+#dl classification doesn't quite work. for this dataset.
+
+
+rf.classifier.full <- h2o.randomForest(
+	x = colnames(h2o.train.features),
+	y = colnames(h2o.train.class),
+	data = h2o.train,
+	classification = TRUE,
+	ntree = 500,
+	depth = 10,
+	balance.classes = TRUE,
+	importance = TRUE,
+	oobee = TRUE
+	# nfolds = 5
+)
+
+
+
+
+
