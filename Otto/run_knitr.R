3b4f7248f7f741f63c72acc207d0beb75e91c98c
ajing <ajingnk@gmail.com>
2015-05-11 14:02:20 -0400

    new added functions for data reparing

--- Otto/run_knitr.R
+++ Otto/run_knitr.R
@@ -1,5 +1,5 @@
 require(knitr) # required for knitting from rmd to md
 require(markdown) # required for md to html
-knit('sup_fig.Rmd', 'test.md') # creates md file
+knit('data_preprocess.Rmd', 'test.md') # creates md file
 markdownToHTML('test.md', 'test.html') # creates html file
 browseURL(paste('file://', file.path(getwd(),'test.html'), sep='')) # open file in browser
