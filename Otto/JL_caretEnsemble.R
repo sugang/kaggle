448807499fbfb7ec8953602c74bd53db63c932fa
ajing <ajingnk@gmail.com>
2015-05-16 23:16:47 -0400

    updated caret ensemble code

--- Otto/JL_caretEnsemble.R
+++ Otto/JL_caretEnsemble.R
@@ -3,6 +3,10 @@ setwd("~/Documents/Github/kaggle_OttoGroup/Data")
 train <- read.csv("./Data/train.csv")
 test <- read.csv("./Data/test.csv")
 
+##### for multiple process
+library(doMC)
+registerDoMC(cores = 4)
+
 ##### caret ensemble package ####
 library("caretEnsemble")
 
@@ -30,8 +34,8 @@ MCLogLoss <- function(data, lev = NULL, model = NULL)  {
 # Pre-define traincontrol for repeated cross validation
 fitControl <- trainControl(
   method = "repeatedcv",
-  number = 10,
-  repeats = 10,
+  number = 3,
+  repeats = 1,
   classProbs = TRUE,
   summaryFunction=MCLogLoss, 
   savePredictions=TRUE,
@@ -39,8 +43,8 @@ fitControl <- trainControl(
   verboseIter = TRUE)
 
 specifiedTuneList =list(
-  rf1=caretModelSpec(method='rf', tuneGrid=data.frame(.mtry=1:5)),
-  rf2=caretModelSpec(method='rf', tuneGrid=data.frame(.mtry=5:10), preProcess='pca'),
+  rf1=caretModelSpec(method='rf', tuneGrid=data.frame(.mtry=2)),
+  rf2=caretModelSpec(method='rf', tuneGrid=data.frame(.mtry=10), preProcess='pca'),
   nn=caretModelSpec(method='nnet', tuneLength=10, trace=FALSE)
 )
 
@@ -50,6 +54,10 @@ model_list <- caretList(
   tuneList=specifiedTuneList,
   methodList=c('rpart', 'nnet')
 )
+caretStack(models, method='glm')
+greedy_ensemble <- caretEnsemble(model_list)
+summary(greedy_ensemble)
+
 
 model_list$glm
 model_list$rpart
@@ -70,3 +78,38 @@ modelCor(resamples(model_list))
 
 greedy_ensemble <- caretEnsemble(model_list)
 summary(greedy_ensemble)
+
+
+
+## for only caret
+library("caret")
+fitControl <- trainControl(
+  method = "repeatedcv",
+  number = 10,
+  repeats = 10,
+  summaryFunction=MCLogLoss, 
+  savePredictions=TRUE,
+  returnData = TRUE,
+  verboseIter = TRUE)
+test_eTree <- train(target~., data=train, method = "extraTrees", trControl = fitControl, preProc = "YeoJohnson", ntree = 200)
+
+
+fitControl <- trainControl(
+  method = "repeatedcv",
+  number = 10,
+  repeats = 10,
+  classProbs = TRUE,
+  summaryFunction=MCLogLoss, 
+  savePredictions=TRUE,
+  returnData = TRUE,
+  verboseIter = TRUE)
+test_eTree <- train(target~., data=train, method = "rf", trControl = fitControl, preProc = "YeoJohnson", ntree = 200)
+
+
+###### Over sampling of minority class
+require(DMwR)
+
+ncols<-ncol(train)
+dm<-cbind(train[,2:ncols],train[,1])
+dmSmote<-SMOTE(target ~ . , dm,k=5,perc.over = 1400,perc.under=140)
+dm<-cbind(dmSmote[ncols],dmSmote[1:ncols-1])
